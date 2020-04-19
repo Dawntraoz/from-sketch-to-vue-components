b7e82e0813c9edc6ee8e0c2d1972476b51565b0b
Alba Silvente Fuentes <36744484+Dawntraoz@users.noreply.github.com>
2020-04-19 19:11:21 +0200

    Open new window with the post

--- src/layout/partials/FooterPartial.vue
+++ src/layout/partials/FooterPartial.vue
@@ -2,6 +2,7 @@
   <footer class="bg-white p-4 text-center">
     <a
       href="https://www.dawntraoz.com/blog/from-sketch-to-vue-components/"
+      target="_blank"
       class="text-sm text-purple-700 underline"
     >This app is an example of this post</a>
   </footer>
